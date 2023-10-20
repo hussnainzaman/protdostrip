09726e1cfa16d6fec3c456c7b60af5682c1080d9
Muskan Kohli <121447070+MuskanKohli2000@users.noreply.github.com>
2023-10-20 00:21:59 -0400

    index file to be edited

--- firebase.ts
+++ firebase.ts
@@ -1,3 +1,5 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
     apiKey: "AIzaSyBOb1mGak7Ifp-QgL6fVOXknJ8kmccnbUo",
     authDomain: "dostrip-f1b21.firebaseapp.com",
@@ -7,3 +9,5 @@ const firebaseConfig = {
     appId: "1:175276502848:web:264f416ae4644fd16414a5",
     measurementId: "G-835MSM95PE"
   };
+  const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
