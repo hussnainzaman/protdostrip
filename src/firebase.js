dfcad78b88fcae79f025769fff2673ba06bc2fd1
Hussnain Uz Zaman <126509706+hussnainzaman@users.noreply.github.com>
2024-04-20 20:28:56 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,14 +6,7 @@ import {getFirestore} from 'firebase/firestore';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBOb1mGak7Ifp-QgL6fVOXknJ8kmccnbUo",
-  authDomain: "dostrip-f1b21.firebaseapp.com",
-  databaseURL: "https://dostrip-f1b21-default-rtdb.firebaseio.com",
-  projectId: "dostrip-f1b21",
-  storageBucket: "dostrip-f1b21.appspot.com",
-  messagingSenderId: "175276502848",
-  appId: "1:175276502848:web:264f416ae4644fd16414a5",
-  measurementId: "G-835MSM95PE"
+  //your API key here
 };
 
 
